80b3f775bfb2795133ebcb416e2c73f37fc5d254
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-04-28 00:00:49 +0500

    Convert Audio To Text

--- services/stt.py
+++ services/stt.py
@@ -1,1 +1,14 @@
 # Groq Whisper (Speech-to-Text)
+
+from groq import Groq
+import os
+
+def transcribe_audio(file_path):
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    with open(file_path, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file=(file_path, file.read()),
+            model = "whisper-large-v3-turbo",
+            response_format = "text",
+        )
+    return transcription
